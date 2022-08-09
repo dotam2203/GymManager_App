8967514c657d2d90b6c7f441a12e954908dec941
Do Tam <dolethanhtam1022@gmail.com>
2022-08-09 17:18:57 +0700

    insert customer final

--- app/src/main/java/com/gym/ui/fragments/TheTapFragment.kt
+++ app/src/main/java/com/gym/ui/fragments/TheTapFragment.kt
@@ -12,7 +12,9 @@ import androidx.lifecycle.lifecycleScope
 import com.google.android.material.tabs.TabLayoutMediator
 import com.gym.R
 import com.gym.databinding.FragmentThetapBinding
+import com.gym.model.GoiTapModel
 import com.gym.model.KhachHangModel
+import com.gym.model.LoaiKhModel
 import com.gym.ui.FragmentNext
 import com.gym.ui.adapter.DangKyViewPagerAdapter
 import com.gym.ui.viewmodel.ViewModel
@@ -25,6 +27,7 @@ class TheTapFragment : FragmentNext() {
     var khachHang = KhachHangModel()
     var receiveKH = KhachHangModel()
     var khachHangs = ArrayList<KhachHangModel>()
+    var loaiKHs = ArrayList<LoaiKhModel>()
     var phones = ArrayList<String>()
     val tabTitle = listOf("Đăng Ký","Đã Đăng Kí")
     override fun onCreateView(
@@ -57,23 +60,117 @@ class TheTapFragment : FragmentNext() {
             btnInfoKH.setOnClickListener {
                 dialogInfoKH(khachHang)
             }
+            btnInsertKH.setOnClickListener {
+                Toast.makeText(requireContext(), "You on click!!", Toast.LENGTH_SHORT).show()
+                dialogInsertKH()
+            }
+        }
+    }
+
+    private fun dialogInsertKH() {
+        val dialog = Dialog(activity!!)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_khachhang)
+
+        val window: Window? = dialog.window
+        window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val windowAtrributes : WindowManager.LayoutParams = window!!.attributes
+        windowAtrributes.gravity = Gravity.CENTER
+        window.attributes = windowAtrributes
+        //click ra bên ngoài để tắt dialog
+        //false = no; true = yes
+        dialog.setCancelable(true)
+        dialog.show()
+        //==============================================================
+        var spLoai: AutoCompleteTextView = dialog.findViewById(R.id.spLoaiKH)
+        var spPhai: AutoCompleteTextView = dialog.findViewById(R.id.spPhaiKH)
+        var txtMaKH: EditText = dialog.findViewById(R.id.txtMaKH)
+        var txtTenKH: EditText = dialog.findViewById(R.id.txtTenKH)
+        var txtEmailKH: EditText = dialog.findViewById(R.id.txtEmailKH)
+        var txtSdtKH: EditText = dialog.findViewById(R.id.txtPhoneKH)
+        var txtDiaChi: EditText = dialog.findViewById(R.id.txtDiaChiKH)
+        //-----------------------------------------------------
+        val btnHuy: Button = dialog.findViewById(R.id.btnHuyKH)
+        val btnThem: Button = dialog.findViewById(R.id.btnThemKH)
+        txtMaKH.visibility = View.GONE
+        //==============================================================
+        var tenLoaiKHs = ArrayList<String>()
+        var phais = listOf("Nam","Nữ")
+        for(i in loaiKHs.indices){
+            tenLoaiKHs.add(loaiKHs[i].tenLoaiKH)
+        }
+        val arrAdapterLoaiKH = ArrayAdapter(requireContext(),R.layout.dropdown_item,tenLoaiKHs)
+        val arrAdapterPhai = ArrayAdapter(requireContext(),R.layout.dropdown_item,phais)
+        var selectLoaiKH: String = ""
+        var idLoaiKH: Int = 0
+        var selectPhai: String = ""
+        //==========================Loại kh==============
+        spLoai.setAdapter(arrAdapterLoaiKH)
+        spLoai.setOnItemClickListener { parent, view, position, id ->
+            selectLoaiKH = parent.getItemAtPosition(position).toString()
+            for(i in loaiKHs.indices){
+                if(selectLoaiKH == loaiKHs[i].tenLoaiKH){
+                    idLoaiKH = loaiKHs[i].idLoaiKH
+                }
+            }
+        }
+        //------------------------------------------------
+        //-----------------------Phái-------------------------
+        spPhai.setAdapter(arrAdapterPhai)
+        spPhai.setOnItemClickListener { parent, view, position, id ->
+            selectPhai = parent.getItemAtPosition(position).toString()
+        }
+        //----------------------------------------------------
+        btnHuy.setOnClickListener {
+            dialog.dismiss()
+        }
+        btnThem.setOnClickListener {
+            Toast.makeText(requireContext(), "maKH: ${randomString("khách hàng", getMaKHMax(khachHangs))} \ntenKH: ${replaceString(txtTenKH.text.toString())} \nphai: $selectPhai \nSDT: ${txtSdtKH.text} \nemail:${txtEmailKH.text} \nDiaChi: ${txtDiaChi.text} \nloaiKH:$idLoaiKH", Toast.LENGTH_LONG).show()
+            viewModel.insertKhachHang(KhachHangModel(randomString("khách hàng", getMaKHMax(khachHangs)),replaceString(txtTenKH.text.toString()),txtEmailKH.text.toString(),txtSdtKH.text.toString(),selectPhai,txtDiaChi.text.toString(),"",idLoaiKH))
+            Toast.makeText(requireContext(), "insert customer successful!!", Toast.LENGTH_SHORT).show()
         }
     }
     fun initVewModel(){
         viewModel.getDSKhachHang()
         lifecycleScope.launchWhenCreated {
             viewModel.khachHangs.collect{ response ->
-                if(response.isEmpty()){
-                    return@collect
-                }
-                else{
+                if(response.isNotEmpty()){
                     khachHangs.addAll(response)
                     for(i in response.indices){
                         phones.add(response[i].sdt)
                     }
                 }
+                else{
+                    return@collect
+                }
             }
         }
+        viewModel.getDSLoaiKH()
+        lifecycleScope.launchWhenCreated {
+            viewModel.loaiKHs.collect{
+                if(it.isNotEmpty()){
+                    loaiKHs.addAll(it)
+                }
+                else
+                    return@collect
+            }
+        }
+    }
+    fun getMaKHMax(khachHangs: ArrayList<KhachHangModel>): String {
+        if (khachHangs.isNotEmpty()) {
+            var max: Int = khachHangs[0].maKH.substring(2).toInt()
+            var maMax = khachHangs[0].maKH
+            for (i in khachHangs.indices) {
+                if (max <= khachHangs[i].maKH.substring(2).toInt()) {
+                    max = khachHangs[i].maKH.substring(2).toInt()
+                    maMax = khachHangs[i].maKH
+                }
+            }
+            return maMax
+        }
+        return "KH00"
     }
     fun setControl(){
         binding.apply {
