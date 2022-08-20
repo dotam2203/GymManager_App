637e7a74026b4f4f893f608f3bf7e90e0eb6b594
Do Tam <dolethanhtam1022@gmail.com>
2022-08-21 03:15:18 +0700

    upload image local ok!

--- app/src/main/java/com/gym/ui/fragments/TaiKhoanFragment.kt
+++ app/src/main/java/com/gym/ui/fragments/TaiKhoanFragment.kt
@@ -1,15 +1,23 @@
 package com.gym.ui.fragments
 
 import android.app.Dialog
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide.init
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.gym.R
 import com.gym.databinding.FragmentTaikhoanBinding
 import com.gym.model.NhanVienModel
@@ -22,6 +30,9 @@ import kotlinx.coroutines.delay
 class TaiKhoanFragment : FragmentNext() {
     private lateinit var binding: FragmentTaikhoanBinding
     private lateinit var sharedPreferencesLogin: SharedPreferencesLogin
+    private lateinit var storageReference: StorageReference
+    private lateinit var imageUri: Uri
+    private var fileName = ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,7 +43,36 @@ class TaiKhoanFragment : FragmentNext() {
         refreshData()
         showDataUser()
         binding.imbSaveAcc.visibility = View.GONE
+        binding.imvUpload.visibility = View.GONE
+        //==========upload image==========
+        val getImage = registerForActivityResult(ActivityResultContracts.GetContent(), ActivityResultCallback {
+            if (it != null) {
+                imageUri = it
+            }
+            binding.imvAvatar.setImageURI(imageUri)
+        })
+        binding.imvUpload.setOnClickListener {
+            getImage.launch("image/*")
+        }
+        //==============================
         return binding.root
+    }
+
+    private fun uploadImage() {
+        val msg = getRandomMaHD()
+        fileName = "avt${msg.substring(2)}"
+        storageReference = FirebaseStorage.getInstance().getReference("image/${fileName}")
+        dialogPopMessage("Uploading file...", R.drawable.ic_load11)
+        lifecycleScope.launchWhenCreated {
+            delay(2000L)
+            storageReference.putFile(imageUri)
+                .addOnSuccessListener {
+                    binding.imvAvatar.setImageURI(imageUri)
+                    Toast.makeText(requireContext(), "Upload avatar successful!", Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener(OnFailureListener {
+                    Toast.makeText(requireContext(), "Upload avatar failed ${it}!", Toast.LENGTH_SHORT).show()
+                })
+        }
     }
 
     private fun init() {
@@ -46,45 +86,50 @@ class TaiKhoanFragment : FragmentNext() {
                 dialogLogout()
             }
             imbEditAcc.setOnClickListener {
+                txtUserAcc.setTextColor(R.color.gray_100)
+                txtEmailAcc.setTextColor(R.color.gray_100)
                 txtPassAcc.isEnabled = true
                 txtPassAcc.requestFocus()
                 txtNameAcc.isEnabled = true
                 txtPhoneAcc.isEnabled = true
                 txtAdressAcc.isEnabled = true
 
+                binding.imvUpload.visibility = View.VISIBLE
                 imbEditAcc.visibility = View.GONE
                 imbSaveAcc.visibility = View.VISIBLE
+
             }
             imbSaveAcc.setOnClickListener {
                 //update data
                 val maQuyen: String = SingletonAccount.taiKhoan?.maQuyen.toString()
                 val maNV: String = SingletonAccount.taiKhoan?.maNV.toString()
                 val user: String = SingletonAccount.taiKhoan?.maTK.toString()
-
+                uploadImage()
                 lifecycleScope.launchWhenCreated {
-                    viewModel.updateNhanVien(NhanVienModel(maNV,txtNameAcc.text.toString(),txtEmailAcc.text.toString(),txtPhoneAcc.text.toString(),"Nữ",txtAdressAcc.text.toString()))
-                    viewModel.nhanVien.collect{
-                        if(it != null){
+                    viewModel.updateNhanVien(NhanVienModel(maNV, txtNameAcc.text.toString(), txtEmailAcc.text.toString(), txtPhoneAcc.text.toString(), "Nữ", txtAdressAcc.text.toString()))
+                    viewModel.nhanVien.collect {
+                        if (it != null) {
                             Toast.makeText(requireContext(), "update staff successful!", Toast.LENGTH_SHORT).show()
-                        }
-                        else
+                        } else
                             Toast.makeText(requireContext(), "update staff failed!", Toast.LENGTH_SHORT).show()
                     }
                 }
                 lifecycleScope.launchWhenCreated {
-                    viewModel.updateTaiKhoan(TaiKhoanModel(user,txtPassAcc.text.toString(),"Hoạt động",maQuyen,maNV))
-                    viewModel.taiKhoan.collect{
-                        if(it != null){
+                    viewModel.updateTaiKhoan(TaiKhoanModel(user, txtPassAcc.text.toString(), "Hoạt động", maQuyen, maNV))
+                    viewModel.taiKhoan.collect {
+                        if (it != null) {
                             Toast.makeText(requireContext(), "update acc successful!", Toast.LENGTH_SHORT).show()
-                            SingletonAccount.taiKhoan = TaiKhoanModel(user,txtPassAcc.text.toString(),"Hoạt động",maQuyen,maNV)
+                            SingletonAccount.taiKhoan = TaiKhoanModel(user, txtPassAcc.text.toString(), "Hoạt động", maQuyen, maNV)
                             showDataUser()
-                        }
-                        else
+                        } else
                             Toast.makeText(requireContext(), "update acc failed!", Toast.LENGTH_SHORT).show()
                     }
                 }
+                txtUserAcc.setTextColor(R.color.black)
+                txtEmailAcc.setTextColor(R.color.black)
                 imbEditAcc.visibility = View.VISIBLE
                 imbSaveAcc.visibility = View.GONE
+                binding.imvUpload.visibility = View.GONE
             }
         }
     }
@@ -98,7 +143,7 @@ class TaiKhoanFragment : FragmentNext() {
         window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
         window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
-        val windowAtrributes : WindowManager.LayoutParams = window!!.attributes
+        val windowAtrributes: WindowManager.LayoutParams = window!!.attributes
         windowAtrributes.gravity = Gravity.CENTER
         window.attributes = windowAtrributes
         //click ra bên ngoài để tắt dialog
@@ -115,7 +160,7 @@ class TaiKhoanFragment : FragmentNext() {
             sharedPreferencesLogin.removeData()
             lifecycleScope.launchWhenCreated {
                 delay(3000L)
-                getFragment(requireView(),R.id.navHomeToLogin)
+                getFragment(requireView(), R.id.navHomeToLogin)
             }
             //replaceFragment(R.id.fragmentLogin, LoginFragment())
             //activity!!.finish()
@@ -125,17 +170,17 @@ class TaiKhoanFragment : FragmentNext() {
         }
     }
 
-    fun showDataUser(){
+    fun showDataUser() {
         binding.apply {
             viewModel.getQuyen(SingletonAccount.taiKhoan?.maQuyen.toString())
             lifecycleScope.launchWhenCreated {
-                viewModel.quyen.collect{ response ->
-                        tvQuyen.text = response?.tenQuyen
+                viewModel.quyen.collect { response ->
+                    tvQuyen.text = response?.tenQuyen
                 }
             }
             viewModel.getNhanVien(SingletonAccount.taiKhoan?.maNV.toString())
             lifecycleScope.launchWhenCreated {
-                viewModel.nhanVien.collect{ response ->
+                viewModel.nhanVien.collect { response ->
                     txtUserAcc.apply {
                         setText(SingletonAccount.taiKhoan?.maTK)
                         isEnabled = false
@@ -164,6 +209,7 @@ class TaiKhoanFragment : FragmentNext() {
             }
         }
     }
+
     private fun refreshData() {
         binding.apply {
             refresh.setOnRefreshListener {
